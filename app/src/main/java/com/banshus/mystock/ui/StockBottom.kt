fcb4151faf2da11be85548c5ebf171500f7d69b6
valar <azgrhl@gmail.com>
2024-07-29 22:17:35 +0800

    更新narbar樣式

--- app/src/main/java/com/banshus/mystock/ui/StockBottom.kt
+++ app/src/main/java/com/banshus/mystock/ui/StockBottom.kt
@@ -17,6 +17,9 @@ import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.QueryStats
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -29,59 +32,68 @@ data class TabData(val icon: ImageVector, val description: String)
 @Composable
 fun NavBar(selected: Int, onTabSelected: (Int) -> Unit) {
     val tabs = listOf(
-        TabData(Icons.Filled.QueryStats, "QueryStats"),
-        TabData(Icons.Filled.CalendarMonth, "CalendarMonth"),
-        TabData(Icons.Filled.Add, "Add"),
-        TabData(Icons.Filled.BarChart, "BarChart"),
-        TabData(Icons.Filled.Menu, "Menu")
+        TabData(Icons.Filled.QueryStats, "持股"),
+        TabData(Icons.Filled.CalendarMonth, "記錄"),
+        TabData(Icons.Filled.Add, "新增"),
+        TabData(Icons.Filled.BarChart, "報表"),
+        TabData(Icons.Filled.Menu, "設定")
     )
-
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .background(MaterialTheme.colorScheme.surfaceContainer)
-            .padding(8.dp)
-    ) {
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceEvenly
-        ) {
-            tabs.forEachIndexed { index, tab ->
-                NavItem(
-                    imageVector = tab.icon,
-                    contentDescription = tab.description,
-                    modifier = Modifier
-                        .size(58.dp)
-                        .padding(10.dp),
-                    tint = if (selected == index) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.onSurface,
-                    onClick = {
-                        onTabSelected(index)
-                    }
-                )
-            }
+    NavigationBar{
+        tabs.forEachIndexed { index, tab ->
+            NavigationBarItem(
+                icon = { Icon(tab.icon, contentDescription = tab.description) },
+                label = { Text(tab.description) },
+                selected = selected == index,
+                onClick = { onTabSelected(index) }
+            )
         }
     }
+//    Box(
+//        modifier = Modifier
+//            .fillMaxWidth()
+//            .background(MaterialTheme.colorScheme.surfaceContainer)
+//            .padding(8.dp)
+//    ) {
+//        Row(
+//            modifier = Modifier.fillMaxWidth(),
+//            horizontalArrangement = Arrangement.SpaceEvenly
+//        ) {
+//            tabs.forEachIndexed { index, tab ->
+//                NavItem(
+//                    imageVector = tab.icon,
+//                    contentDescription = tab.description,
+//                    modifier = Modifier
+//                        .size(58.dp)
+//                        .padding(10.dp),
+//                    tint = if (selected == index) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.onSurface,
+//                    onClick = {
+//                        onTabSelected(index)
+//                    }
+//                )
+//            }
+//        }
+//    }
 }
 
 
 
-@Composable
-fun NavItem(
-    imageVector: ImageVector,
-    contentDescription: String,
-    modifier: Modifier,
-    tint: Color = MaterialTheme.colorScheme.onPrimary,
-    onClick: () -> Unit
-) {
-    Column(modifier = Modifier.clickable(onClick = onClick)) {
-        Icon(
-            imageVector = imageVector,
-            contentDescription = contentDescription,
-            modifier = modifier,
-            tint = tint
-        )
-    }
-}
+//@Composable
+//fun NavItem(
+//    imageVector: ImageVector,
+//    contentDescription: String,
+//    modifier: Modifier,
+//    tint: Color = MaterialTheme.colorScheme.onPrimary,
+//    onClick: () -> Unit
+//) {
+//    Column(modifier = Modifier.clickable(onClick = onClick)) {
+//        Icon(
+//            imageVector = imageVector,
+//            contentDescription = contentDescription,
+//            modifier = modifier,
+//            tint = tint
+//        )
+//    }
+//}
 
 @Preview(showBackground = false)
 @Composable
